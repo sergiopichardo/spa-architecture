46abc0a40b8b7926e2e1cacb280fab13d04ae7a5
Sergio Pichardo <sergiopichardo@users.noreply.github.com>
2023-10-08 15:34:12 -0500

    feat(s3): conditionally create s3 bucket, only if the assets path is provided

--- lib/object-storage-stack.ts
+++ lib/object-storage-stack.ts
@@ -1,35 +1,43 @@
+import { Construct } from 'constructs'; 
 import * as cdk from 'aws-cdk-lib';
 import * as s3 from 'aws-cdk-lib/aws-s3';
 import * as s3deploy from 'aws-cdk-lib/aws-s3-deployment';
-import { Construct } from 'constructs';
+import * as fs from 'fs';
 
-interface ObjectStorageProps extends cdk.NestedStackProps {
+interface ObjectStorageNestedStackProps extends cdk.NestedStackProps {
     buildAssetsPath: string;
 }
 
-export class ObjectStorageNestedStack extends cdk.NestedStack {
-  public websiteBucket: s3.IBucket;
+export default class ObjectStorageNestedStack extends cdk.NestedStack {
+    public originBucket: s3.IBucket;
 
-  constructor(scope: Construct, id: string, props: ObjectStorageProps) {
-    super(scope, id, props);
+    constructor(scope: Construct, id: string, props: ObjectStorageNestedStackProps) {
+        super(scope, id, props);
 
-    this.websiteBucket = this.createOriginBucket();
-    this.createDeployment(this.websiteBucket, props.buildAssetsPath);
-  }
+        this.originBucket = this.createOriginBucket();
 
-  createOriginBucket() {
-    return new s3.Bucket(this, 'UiOriginBucket', {
-      blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
-      removalPolicy: cdk.RemovalPolicy.DESTROY,
-      autoDeleteObjects: true,
-      publicReadAccess: false,
-    });
-  }
+        if (!fs.existsSync(props.buildAssetsPath)) {
+            throw new Error("A build path to SPA static assets must be provided")
+        }
 
-  createDeployment(websiteBucket: s3.IBucket, buildAssetsPath: string) {
-    new s3deploy.BucketDeployment(this, 'UiOriginDeployment', {
-      sources: [s3deploy.Source.asset(buildAssetsPath)],
-      destinationBucket: websiteBucket
-    });
-  }
+        this.createDeployment(props.buildAssetsPath);
+    }
+
+    createOriginBucket() {
+        return new s3.Bucket(this, 'OriginBucket', {
+            blockPublicAccess: s3.BlockPublicAccess.BLOCK_ALL,
+            removalPolicy: cdk.RemovalPolicy.DESTROY,
+            autoDeleteObjects: true,
+            publicReadAccess: false,
+        });
+    }
+    
+    createDeployment(buildAssetsPath: string) {
+        new s3deploy.BucketDeployment(this, 'OriginDeployment', {
+            sources: [s3deploy.Source.asset(buildAssetsPath)],
+            destinationBucket: this.originBucket
+        });
+    }
+
+
 }
